6a7517bf8609966e54ba1c97b5ec9c0399fbcb6b
Gilberto Bermúdez Garro <gbermude@outlook.com>
2022-12-14 08:57:39 -0600

    Update

--- bloque2/20221214/demo9/src/main.tsx
+++ bloque2/20221214/demo9/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-icons/font/bootstrap-icons.css'
