2d582a16d33e9d923458f7a251713c09f607e659
Gilberto Bermúdez Garro <gbermude@outlook.com>
2022-12-15 09:42:36 -0600

    Update cartSlide.ts

--- bloque2/20221215/demo10/src/data/cartSlide.ts
+++ bloque2/20221215/demo10/src/data/cartSlide.ts
@@ -1,1 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlide = createSlice({
+    name: 'cart',
+    initialState: {
+        items: Array()
+    },
+    reducers: {
+        addToCart : (state, action) => {
+            state.items = [action.payload, ...state.items]
+        },
+        removeFromCart : (state, action) => {
+            state.items = state.items.filter(i => i !== action.payload)
+        },
+        clearCart : (state, action) => {
+            state.items = []
+        }
+    }
+})
+
+export default cartSlide
