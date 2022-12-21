a75238635aa324c54a8732285767572c2e7cb591
Gilberto Bermúdez Garro <gbermude@outlook.com>
2022-12-20 19:55:11 -0600

    Update

--- bloque3/20221220/ndemo10/model/tools.ts
+++ bloque3/20221220/ndemo10/model/tools.ts
@@ -4,4 +4,4 @@ import bodyParser from "body-parser";
 /** 
  * Convertir un body de un IncomingMessage a un JSON
 */
-export const getBody = promisify(bodyParser.urlencoded());
+export const getBody = promisify(bodyParser.urlencoded({extended: true}));
